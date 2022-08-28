75412eb5fe5788611ea543ba637d6f93209de5c8
OliverosA <aldair_oliveros09@hotmail.es>
2022-08-27 21:59:33 -0600

    udpating Transactions.js component

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,65 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import Container from 'react-bootstrap/esm/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from 'react';
 import Table from 'react-bootstrap/Table';
-import Chart from './Chart';
+import Btn from './Btn';
 
 const Transactions = (props) => {
-  const [transaction, setTransaction] = useState([]);
-
-  // options to fetch the api
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: 'Bearer <TOKEN>',
-    },
-  };
-
-  useEffect(() => {
-    // get the transaction from the example API
-    const getTransactions = async () => {
-      const url = `https://dev.lunchmoney.app/v1/transactions`;
-      const response = await fetch(url, options);
-      const jsonData = await response.json();
-      setTransaction(jsonData?.transactions);
-    };
-
-    getTransactions();
-  }, []);
-
   const getAllTransactions = () => {
-    return transaction?.map((item) => (
+    return props?.transactions?.map((item) => (
       <tbody>
         <tr key={item.id}>
           <td>{item.date}</td>
-          <td>{item.payee}</td>
+          <td>{item.name}</td>
           <td>{`$${item.amount}`}</td>
+          <td>
+            <Btn text={'Delete'} variant={'danger'} />
+          </td>
         </tr>
       </tbody>
     ));
   };
 
   return (
-    <>
-      <Chart />
-      <Container className='my-5'>
-        <Row>
-          <Col xs={8}>
-            <Table striped bordered hover variant='dark'>
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Name</th>
-                  <th>Amount</th>
-                </tr>
-              </thead>
-              {getAllTransactions()}
-            </Table>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <div className='tableLayout'>
+      <Table striped bordered hover variant='dark'>
+        <thead>
+          <tr key={'table-headers'}>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Amount</th>
+            <th>Options</th>
+          </tr>
+        </thead>
+        {getAllTransactions()}
+      </Table>
+    </div>
   );
 };
 
