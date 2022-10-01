00ab08c5f9f6d1d850e560ed7008dc41371e6fd0
OliverosA <aldair_oliveros09@hotmail.es>
2022-09-30 18:25:13 -0600

    create bankAccountTransactions state

---
+++ src/store/slices/transaction/transactionSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  transactions: {},
+  bankAccountTransactions: {},
+};
+
+export const trasactionSlice = createSlice({
+  name: "bankaccount",
+  initialState,
+  reducers: {
+    setAllTransactions: (state, action) => {
+      state.transactions = action.payload;
+    },
+    setBankAccountTransactions: (state, action) => {
+      state.bankAccountTransactions = action.payload;
+    },
+  },
+});
+
+export const { setAllTransactions, setBankAccountTransactions } =
+  trasactionSlice.actions;
+
+export default trasactionSlice.reducer;
